87948cb51d00f4749582a79bfe4d889657eb7e85
tkduman <kaan@dumanmail.com>
2018-09-14 19:56:48 +0300

    added css

--- index.php
+++ index.php
@@ -8,6 +8,12 @@
         height: auto;
       }
     </style>
+    <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+    <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+    <link rel="stylesheet" type="text/css" href="css/util.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
   </head>
   <body>
     <div class="chart-container">
