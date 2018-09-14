709b1755d0601b5814c5d3ec46ffd30d1ae17de6
tkduman <kaan@dumanmail.com>
2018-09-14 22:16:11 +0300

    test

---
+++ adddata.php
@@ -0,0 +1,22 @@
+<?php
+$link = mysqli_connect("localhost", "admin_fmbin", "anka0606ankA", "admin_fmbin");
+
+if($link === false){
+	die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+
+$player_id = mysqli_real_escape_string($link, $_REQUEST['player_id']);
+$price = mysqli_real_escape_string($link, $_REQUEST['price']);
+$price = str_replace(array('.', ','), '' , $price);
+$date = date("Y-m-d H:i:s", $current_timestamp);
+
+$sql = "INSERT INTO players (player_id, price_value) VALUES ('$player_id','$price')"; //2nd value represents player_id, should be dynamic
+
+if(mysqli_query($link, $sql)){
+	echo "Records added successfully.";
+} else{
+	echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+
+mysqli_close($link);
+?>
---
+++ js/linegraph.js
@@ -0,0 +1,54 @@
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+$(document).ready(function(){
+  var player_id = getParameterByName('player_id');
+  $.ajax({
+    url : "../data.php?player_id=" + player_id,
+    type : "GET",
+    success : function(data){
+      console.log(data);
+
+      var last_report = [];
+      var price_value = [];
+
+      for(var i in data) {
+        last_report.push(data[i].last_report);
+        price_value.push(data[i].price_value);
+      }
+
+      var chartdata = {
+        labels: last_report,
+        datasets: [
+          {
+            label: "Price",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(59, 89, 152, 0.75)",
+            borderColor: "rgba(59, 89, 152, 1)",
+            pointHoverBackgroundColor: "rgba(59, 89, 152, 1)",
+            pointHoverBorderColor: "rgba(59, 89, 152, 1)",
+            data: price_value
+          }
+        ]
+      };
+
+      var ctx = $("#mycanvas");
+
+      var LineGraph = new Chart(ctx, {
+        type: 'line',
+        data: chartdata
+      });
+    },
+    error : function(data) {
+
+    }
+  });
+});
