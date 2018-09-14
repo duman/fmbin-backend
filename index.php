8c4f4d79e09c84243ed6a6bc5e5fb7c49593228a
tkduman <kaan@dumanmail.com>
2018-09-14 19:38:14 +0300

    added jquery

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
     </div>
     
     <!-- javascript -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.min.js"></script>
     <script type="text/javascript" src="js/linegraph.js"></script>
   </body>
