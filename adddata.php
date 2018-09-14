3bec9c2f0e28f224400fca837596eb138014cb8c
tkduman <kaan@dumanmail.com>
2018-09-14 20:22:23 +0300

    fixed a date problem

--- adddata.php
+++ adddata.php
@@ -8,7 +8,7 @@ if($link === false){
 $price = mysqli_real_escape_string($link, $_REQUEST['price']);
 $date = date("Y-m-d H:i:s", $current_timestamp);
 
-$sql = "INSERT INTO players (id, player_id, price_value, last_report) VALUES ('0', '1','$price','$date')"; //2nd value represents player_id, should be dynamic
+$sql = "INSERT INTO players (id, player_id, price_value) VALUES ('0', '1','$price')"; //2nd value represents player_id, should be dynamic
 
 if(mysqli_query($link, $sql)){
 	echo "Records added successfully.";
