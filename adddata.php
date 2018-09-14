025187f871389219aa22874947c380c82418571e
tkduman <kaan@dumanmail.com>
2018-09-14 20:41:48 +0300

    removed a stupid check

--- adddata.php
+++ adddata.php
@@ -1,22 +1,20 @@
 <?php
-if(isset($_POST['price'])) {
-	$link = mysqli_connect("localhost", "admin_fmbin", "anka0606ankA", "admin_fmbin");
+$link = mysqli_connect("localhost", "admin_fmbin", "anka0606ankA", "admin_fmbin");
 
-	if($link === false){
-		die("ERROR: Could not connect. " . mysqli_connect_error());
-	}
-
-	$price = mysqli_real_escape_string($link, $_REQUEST['price']);
-	$date = date("Y-m-d H:i:s", $current_timestamp);
-
-	$sql = "INSERT INTO players (player_id, price_value) VALUES ('1','$price')"; //2nd value represents player_id, should be dynamic
-
-	if(mysqli_query($link, $sql)){
-		echo "Records added successfully.";
-	} else{
-		echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
-	}
-
-	mysqli_close($link);
+if($link === false){
+	die("ERROR: Could not connect. " . mysqli_connect_error());
 }
+
+$price = mysqli_real_escape_string($link, $_REQUEST['price']);
+$date = date("Y-m-d H:i:s", $current_timestamp);
+
+$sql = "INSERT INTO players (player_id, price_value) VALUES ('1','$price')"; //2nd value represents player_id, should be dynamic
+
+if(mysqli_query($link, $sql)){
+	echo "Records added successfully.";
+} else{
+	echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+
+mysqli_close($link);
 ?>
