64aeabdf1a08563791a149e6ad11563d855179ac
tkduman <kaan@dumanmail.com>
2018-09-14 20:18:07 +0300

    improved way of adding price

--- adddata.php
+++ adddata.php
@@ -1,20 +1,19 @@
-<html>
-	<body>
-		<?php
-		$con = mysql_connect("localhost", "admin_fmbin", "anka0606ankA");
+<?php
+$link = mysqli_connect("localhost", "admin_fmbin", "anka0606ankA", "admin_fmbin");
+// Check connection
+if($link === false){
+	die("ERROR: Could not connect. " . mysqli_connect_error());
+}
 
-		if (!$con) {
-			die('Could not connect: ' . mysql_error());
-		}
+$price = mysqli_real_escape_string($link, $_REQUEST['price']);
 
-		mysql_select_db("admin_fmbin", $con);
-		$sql = "INSERT INTO players (id, player_id, price_value, last_report) VALUES ('null', '1','$_POST[price]','CURRENT_TIMESTAMP')"; //2nd value represents player_id, should be dynamic
+$sql = "INSERT INTO players (id, player_id, price_value, last_report) VALUES ('null', '1','$price','CURRENT_TIMESTAMP')"; //2nd value represents player_id, should be dynamic
 
-		if (!mysql_query($sql, $con)) {
-			die('Error: ' . mysql_error());
-		}
+if(mysqli_query($link, $sql)){
+	echo "Records added successfully.";
+} else{
+	echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
 
-		mysql_close($con);
-		?>
-	</body>
-</html>
+mysqli_close($link);
+?>
