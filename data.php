4c1f69f2867ff00cd4293f247d43da55cbb41461
tkduman <kaan@dumanmail.com>
2018-09-14 21:33:12 +0300

    added a way to get specific player data

--- data.php
+++ data.php
@@ -11,12 +11,20 @@ define('DB_NAME', 'admin_fmbin');
 //get connection
 $mysqli = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
+$player_id = mysqli_real_escape_string($mysqli, $_REQUEST['player_id']);
+
 if(!$mysqli){
   die("Connection failed: " . $mysqli->error);
 }
 
 //query to get data from the table
-$query = sprintf("SELECT player_id, price_value, last_report FROM players"); // add WHERE = player_id = "434" to make it runnable with any distinct player
+$sql = "SELECT player_id, price_value, last_report FROM players";
+if(!empty($player_id)) {
+	$sql .= "WHERE player_id = " . $player_id;
+}
+$query = sprintf($sql);
+// add WHERE = player_id = "434" to make it runnable with any distinct player
+// add WHERE = time_interval_from_user so that only data between certain date/time will be displayed
 
 //execute query
 $result = $mysqli->query($query);
--- js/linegraph.js
+++ js/linegraph.js
@@ -1,6 +1,17 @@
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
 $(document).ready(function(){
+  var player_id = getParameterByName('player_id');
   $.ajax({
-    url : "../data.php",
+    url : "../data.php?" + player_id,
     type : "GET",
     success : function(data){
       console.log(data);
