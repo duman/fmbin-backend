8cbb93793ece40c7dcc3a27a7666b6ca626936c2
tkduman <kaan@dumanmail.com>
2018-09-15 12:04:16 +0300

    attempt to create a namespace

--- index.php
+++ index.php
@@ -109,47 +109,7 @@
             data: $('form').serialize(),
             success: function() {
               console.log("Data has been added successfully");
-              document.getElementById("mycanvas").outerHTML = "";
-              var player_id = getParameterByName('player_id');
-              $.ajax({
-                url : "../data.php?player_id=" + player_id,
-                type : "GET",
-                success : function(data){
-                  var last_report = [];
-                  var price_value = [];
-
-                  for(var i in data) {
-                    last_report.push(data[i].last_report);
-                    price_value.push(data[i].price_value);
-                  }
-
-                  var chartdata = {
-                    labels: last_report,
-                    datasets: [
-                      {
-                        label: "Price",
-                        fill: false,
-                        lineTension: 0.1,
-                        backgroundColor: "rgba(59, 89, 152, 0.75)",
-                        borderColor: "rgba(59, 89, 152, 1)",
-                        pointHoverBackgroundColor: "rgba(59, 89, 152, 1)",
-                        pointHoverBorderColor: "rgba(59, 89, 152, 1)",
-                        data: price_value
-                      }
-                    ]
-                  };
-
-                  var ctx = $("#mycanvas");
-
-                  var LineGraph = new Chart(ctx, {
-                    type: 'line',
-                    data: chartdata
-                  });
-                },
-                error : function(data) {
-
-                }
-              });
+              DumanCHART.LineGraph.update();
             },
             error: function() {
               console.log("Could not add the data");
--- js/linegraph.js
+++ js/linegraph.js
@@ -8,24 +8,23 @@ function getParameterByName(name, url) {
     return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
 
+var DumanCHART = {};
 $(document).ready(function(){
   var player_id = getParameterByName('player_id');
   $.ajax({
     url : "../data.php?player_id=" + player_id,
     type : "GET",
     success : function(data){
-      console.log(data);
-
-      var last_report = [];
-      var price_value = [];
+      DumanCHART.last_report = [];
+      DumanCHART.price_value = [];
 
       for(var i in data) {
-        last_report.push(data[i].last_report);
-        price_value.push(data[i].price_value);
+        DumanCHART.last_report.push(data[i].last_report);
+        DumanCHART.price_value.push(data[i].price_value);
       }
 
-      var chartdata = {
-        labels: last_report,
+      DumanCHART.chartdata = {
+        labels: DumanCHART.last_report,
         datasets: [
           {
             label: "Price",
@@ -35,20 +34,19 @@ $(document).ready(function(){
             borderColor: "rgba(59, 89, 152, 1)",
             pointHoverBackgroundColor: "rgba(59, 89, 152, 1)",
             pointHoverBorderColor: "rgba(59, 89, 152, 1)",
-            data: price_value
+            data: DumanCHART.price_value
           }
         ]
       };
 
-      var ctx = $("#mycanvas");
+      DumanCHART.ctx = $("#mycanvas");
 
-      var LineGraph = new Chart(ctx, {
+      DumanCHART.LineGraph = new Chart(DumanCHART.ctx, {
         type: 'line',
-        data: chartdata
+        data: DumanCHART.chartdata
       });
     },
     error : function(data) {
-
     }
   });
 });
