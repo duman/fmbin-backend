70592a0cce72549b21e7ca6a784d444140ec26a6
tkduman <kaan@dumanmail.com>
2018-09-15 12:31:13 +0300

    let's remove old graph and redraw a whole new one

--- index.php
+++ index.php
@@ -109,7 +109,46 @@
             data: $('form').serialize(),
             success: function() {
               console.log("Data has been added successfully");
-              LineGraph.update();
+              LineGraph.destroy();
+                var player_id = getParameterByName('player_id');
+                $.ajax({
+                  url : "../data.php?player_id=" + player_id,
+                  type : "GET",
+                  success : function(data){
+                    last_report = [];
+                    price_value = [];
+
+                    for(var i in data) {
+                      last_report.push(data[i].last_report);
+                      price_value.push(data[i].price_value);
+                    }
+
+                    chartdata = {
+                      labels: last_report,
+                      datasets: [
+                        {
+                          label: "Price",
+                          fill: false,
+                          lineTension: 0.1,
+                          backgroundColor: "rgba(59, 89, 152, 0.75)",
+                          borderColor: "rgba(59, 89, 152, 1)",
+                          pointHoverBackgroundColor: "rgba(59, 89, 152, 1)",
+                          pointHoverBorderColor: "rgba(59, 89, 152, 1)",
+                          data: price_value
+                        }
+                      ]
+                    };
+
+                    ctx = $("#mycanvas");
+
+                    LineGraph = new Chart(ctx, {
+                      type: 'line',
+                      data: chartdata
+                    });
+                  },
+                  error : function(data) {
+                  }
+                });
             },
             error: function() {
               console.log("Could not add the data");
