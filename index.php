fe02a7bd4c4d4b262f7210710bae1e7362469ad2
tkduman <kaan@dumanmail.com>
2018-09-15 11:55:44 +0300

    even crazier solution attempt

--- index.php
+++ index.php
@@ -109,35 +109,45 @@
             data: $('form').serialize(),
             success: function() {
               console.log("Data has been added successfully");
-              var last_report = [];
-              var price_value = [];
+              var player_id = getParameterByName('player_id');
+              $.ajax({
+                url : "../data.php?player_id=" + player_id,
+                type : "GET",
+                success : function(data){
+                  var last_report = [];
+                  var price_value = [];
 
-              for(var i in data) {
-                last_report.push(data[i].last_report);
-                price_value.push(data[i].price_value);
-              }
-
-              var chartdata = {
-                labels: last_report,
-                datasets: [
-                  {
-                    label: "Price",
-                    fill: false,
-                    lineTension: 0.1,
-                    backgroundColor: "rgba(59, 89, 152, 0.75)",
-                    borderColor: "rgba(59, 89, 152, 1)",
-                    pointHoverBackgroundColor: "rgba(59, 89, 152, 1)",
-                    pointHoverBorderColor: "rgba(59, 89, 152, 1)",
-                    data: price_value
+                  for(var i in data) {
+                    last_report.push(data[i].last_report);
+                    price_value.push(data[i].price_value);
                   }
-                ]
-              };
 
-              var ctx = $("#mycanvas");
+                  var chartdata = {
+                    labels: last_report,
+                    datasets: [
+                      {
+                        label: "Price",
+                        fill: false,
+                        lineTension: 0.1,
+                        backgroundColor: "rgba(59, 89, 152, 0.75)",
+                        borderColor: "rgba(59, 89, 152, 1)",
+                        pointHoverBackgroundColor: "rgba(59, 89, 152, 1)",
+                        pointHoverBorderColor: "rgba(59, 89, 152, 1)",
+                        data: price_value
+                      }
+                    ]
+                  };
 
-              var LineGraph = new Chart(ctx, {
-                type: 'line',
-                data: chartdata
+                  var ctx = $("#mycanvas");
+
+                  var LineGraph = new Chart(ctx, {
+                    type: 'line',
+                    data: chartdata
+                  });
+                },
+                error : function(data) {
+
+                }
               });
             },
             error: function() {
