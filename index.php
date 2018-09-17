6239acc0f18a6afc42c262612bc59b7415c8ecd9
tkduman <kaan@dumanmail.com>
2018-09-17 20:20:48 +0300

    improved submit button

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
     <script type="text/javascript" src="js/cardinfo.js"></script>
   </head>
   <body class="center">
-      <form class="input100">
+      <form style="display: inline-block;">
         <select name="time">
           <option value="1">Hourly</option>
           <option value="6">6 Hours</option>
@@ -29,7 +29,9 @@
           <option value="672">1 Month</option>
           <option value="2016">3 Months</option>
         </select>
-        <input type="submit" class="flex-c-m s2-txt2 size4 bg1 bor1 hov1 trans-04">
+        <button type="submit" class="flex-c-m s2-txt2 size4 bg1 bor1 hov1 trans-04" id="submit-time" name="submit-time" type="submit">
+          Select
+        </button>
       </form>
       <div class="chart-container" id="chart-container">
         <canvas id="mycanvas"></canvas>
