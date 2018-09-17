ad45c4f3b4c4ab5647869c61fa4567e37239e80f
tkduman <kaan@dumanmail.com>
2018-09-17 19:34:41 +0300

    Added styling for legend

--- index.php
+++ index.php
@@ -179,7 +179,7 @@
                             legendHtml.push('<ul>');
                             legendHtml.push('<li>');
                             legendHtml.push('<span class="chart-legend" style="background-color: #3b5998"></span>');
-                            legendHtml.push('<span class="chart-legend-label-text">Data is shown for the last ' + time + ' hours</span>');
+                            legendHtml.push('<span class="chart-legend-label-text"><p class="m1-txt1 p-b-36" style="padding-top: 5em; text-align: center;">Data is shown for the last ' + time + ' hours</p></span>');
                             legendHtml.push('</li>');
                             legendHtml.push('</ul>');
                             return legendHtml.join("");
