de29bd5b8e0979dc494d4f38f551079a679374ce
tkduman <kaan@dumanmail.com>
2018-09-17 23:07:11 +0300

    disable zoom

--- index.php
+++ index.php
@@ -19,6 +19,8 @@ if (session_id() == '') {
 <html>
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="user-scalable=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
     <title>BETA</title>
     <style>
       .chart-container {
